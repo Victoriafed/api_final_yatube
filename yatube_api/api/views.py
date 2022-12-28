43d985978556ae6da925ac4e9b4aac719c709c4c
Виктория Федорова <you@example.com>
2022-12-29 00:50:54 +0300

    v2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -11,6 +11,7 @@ from .serializers import (
 )
 from posts.models import Post, Group, Follow
 
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
